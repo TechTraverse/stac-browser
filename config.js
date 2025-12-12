3783b7d9056264dc216cb4836a54b9d2feb5390a
christopher.esterlein <christopher.esterlein@noaa.gov>
2025-12-12 09:19:23 +0000

    custom config, updated workflow

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ module.exports = {
     displayOverview: true,
     buildTileUrlTemplate: null,
     getMapSourceOptions: null,
-    pathPrefix: "/",
+    pathPrefix: "/stac/browser/",
     historyMode: "history",
     cardViewMode: "cards",
     cardViewSort: "asc",
@@ -51,7 +51,37 @@ module.exports = {
     requestHeaders: {},
     requestQueryParameters: {},
     socialSharing: ['email', 'bsky', 'mastodon', 'x'],
-    preprocessSTAC: null,
+    preprocessSTAC: (stac, state) => {
+        const API_GATEWAY_PATTERN = /https?:\/\/[^/]+\.execute-api\.[^/]+/i;
+        const PROXY_BASE = 'https://data-dev.nesdis.noaa.gov/stac/api';
+        
+        if (stac && Array.isArray(stac.links)) {
+            stac.links = stac.links.map(link => {
+                if (link.href && API_GATEWAY_PATTERN.test(link.href)) {
+                    try {
+                        const url = new URL(link.href);
+                        const path = url.pathname;
+                        const search = url.search;
+                        const hash = url.hash;
+                        
+                        // Ensure path starts with /stac/api
+                        const normalizedPath = path.startsWith('/stac/api') 
+                            ? path 
+                            : `/stac/api${path.startsWith('/') ? path : '/' + path}`;
+                        
+                        // Reconstruct URL with proxy domain
+                        link.href = `${PROXY_BASE}${normalizedPath}${search}${hash}`;
+                    } catch (e) {
+                        // If URL parsing fails, skip this link
+                        console.warn('Failed to rewrite URL:', link.href, e);
+                    }
+                }
+                return link;
+            });
+        }
+        
+        return stac;
+    },
     authConfig: null,
     crs: {}
 };
